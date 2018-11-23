d7703f84e41deba85b0a440181bfd88c07e828c6
mistsne <mistsne@outlook.com>
2018-11-23 21:01:47 +0800

    更新打砖块游戏

---
+++ Assets/Scripts/FireBullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FireBullet : MonoBehaviour {
+
+    public GameObject bullet;
+    public float speed = 25f;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Game Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetMouseButton(0)) {
+            GameObject b = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody rgd = b.GetComponent<Rigidbody>();
+            rgd.velocity = transform.forward * speed;
+        }
+	}
+}
